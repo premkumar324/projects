eec1fd8e818ddc4da1ea7450ea05bc89b3dc6fff
Premkumar lande <premkumarvitthallande@example.com>
2025-01-03 22:30:46 +0530

    yup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'light-grey':'#ecf0f1'
+      }
+    },
+  },
+  plugins: [],
+}
+
